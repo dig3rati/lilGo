f8920fbad86b070ec38cf81e2e0a812e35cdc8b4
Bhaskar Nidumukalla <bhaskar@denaglobal.com>
2013-08-10 20:43:50 -0700

    initial commit

---
+++ goLil.go
@@ -0,0 +1,9 @@
+package goLil
+
+func Shorten(longURL string) (shortURL string) {
+
+}
+
+func Expand(shortURL string) (longURL string) {
+
+}
---
+++ goLil_test.go
@@ -0,0 +1,17 @@
+package goLil
+
+import (
+	"testing"
+)
+
+func TestShorten(t *testing.T) {
+
+}
+
+func TestExpand(t *testing.T) {
+
+}
+
+func randomURL(length int) {
+
+}
